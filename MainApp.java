a3cc72ddc68de063edf07c165dc73ff8237ba8c9
nesky26 <99383369+nesky26@users.noreply.github.com>
2022-02-10 10:44:40 +0300

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.lesson1;
+
+public class MainApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5, b = -10;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 0;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers() {
+        int a=1345, b=4342;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+}
